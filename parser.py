98e8844b2932bb6a590aab2f83e32a0c83db5a7c
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-03-22 19:32:28 +0100

    Fix numpy with Fraction.

--- parser.py
+++ parser.py
@@ -65,8 +65,8 @@ class Parser:
                 continue
             else:
                 raise Exception('Syntax error at line %s.' % lineno)
-        self.linearProgram.tableaux = numpy.empty((self.linearProgram.nbConstraints + 1, self.linearProgram.nbVariables + self.linearProgram.nbConstraints + 2))
-        self.linearProgram.tableaux[:] = Fraction(0, 1)
+        self.linearProgram.tableaux = numpy.matrix([[Fraction(0, 1)]*(self.linearProgram.nbVariables + self.linearProgram.nbConstraints + 2)\
+            for i in range(self.linearProgram.nbConstraints + 1)])
 
     def newExpression(self, lineno, constraintID, content, objective):
         op = self.COMP_EQ_REGEXP.findall(content)
