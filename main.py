7114c3ccb3f25e2b71ff400256ee75a1722f41dc
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-03-31 20:02:05 +0200

    Better main.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-
+import argparse
 from simplex import LinearProgram, Parser
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            description='Simplex algorithm, to solve linear programs.')
+    parser.add_argument('inputfile')
+    parser.add_argument('-o', '--output', type=str,
+            default='', help='Output file (default: standard output).')
+    parser.add_argument('-v', '--verbose', action='store_true',
+            help='Display informations during the solving of the program.')
+    args = parser.parse_args()
     lp = LinearProgram()
-    parser = Parser(lp, 'example2.in')
+    parser = Parser(lp, args.inputfile)
     parser.parse()
-    print(lp.solve(True))
+    print(lp.solve(args.verbose))
