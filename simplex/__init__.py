6382164660edd3a10ff74c3c0bbd744bc909eb20
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-04-16 17:54:56 +0200

    structure.py → simplex.py

--- simplex/__init__.py
+++ simplex/__init__.py
@@ -1,4 +1,4 @@
-from .structure import LinearProgram, EndOfAlgorithm, Unbounded, Empty
+from .simplex import LinearProgram, EndOfAlgorithm, Unbounded, Empty
 from .parser import Parser
 
 __all__ = ['LinearProgram, EndOfAlgorithm, Unbounded', 'Empty', 'Parser']
